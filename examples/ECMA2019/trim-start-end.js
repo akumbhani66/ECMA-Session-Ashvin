cb817ce0784cd35469ac120564d4b6174c388fe9
akumbhani66 <akumbhani66@gmail.com>
2020-01-22 16:42:46 +0530

    ECMA 2019 along with examples

---
+++ examples/ECMA2019/trim-start-end.js
@@ -0,0 +1,17 @@
+const exmpl = '   example of trim start!   ';
+
+console.log(exmpl);
+// expected output: "   example of trim start!   ";
+
+console.log(exmpl.trimStart());
+// expected output: "example of trim start!   ";
+
+
+const exmpl2 = '   example of trim end!   ';
+
+console.log(exmpl);
+// expected output: "   example of trim end!   ";
+
+console.log(exmpl.trimEnd());
+// expected output: "   example of trim end!";
+
